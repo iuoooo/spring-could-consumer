4977d5d785c4ff63880ffbfaa19f9d3223e0ca05
iuoooo <javalijingkui@163.com>
2020-01-15 14:11:33 +0800

    消费者(consumer service)服务初始化

---
+++ test-consumer/src/main/java/com/iuoooo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.iuoooo.controller;
+
+import com.iuoooo.remote.HelloRemote;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class HelloController {
+    @Resource
+    HelloRemote helloRemote;
+
+    @RequestMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return helloRemote.hello(name);
+    }
+}
