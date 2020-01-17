7701702abff5a6c70de29c30156b0a0668a95ada
iuoooo <javalijingkui@163.com>
2020-01-17 16:59:17 +0800

    消费者(consumer service)服务 添加熔断机制  当你访问http:localhost:9000/get/*时，出现异常，调用熔断返回执行方法

--- test-consumer/src/main/java/com/iuoooo/controller/HelloController.java
+++ test-consumer/src/main/java/com/iuoooo/controller/HelloController.java
@@ -1,11 +1,14 @@
 package com.iuoooo.controller;
 
 import com.iuoooo.remote.HelloRemote;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Random;
 
 @RestController
 public class HelloController {
@@ -15,5 +18,18 @@ public class HelloController {
     @RequestMapping("/hello/{name}")
     public String index(@PathVariable("name") String name) {
         return helloRemote.hello(name);
+    }
+
+    @GetMapping("/get/{id}")
+    @HystrixCommand(fallbackMethod = "queryUserByIdFallback")
+    public String queryUserById(Long id){
+        // 为了演示超时现象，我们在这里然线程休眠,时间随机 0~2000毫秒
+        long l = id / 0;
+        return "success";
+    }
+
+    public String queryUserByIdFallback(Long id){
+        String str="紧急处理返回";
+        return str;
     }
 }
